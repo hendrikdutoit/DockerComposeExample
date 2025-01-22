f20008944c2f82b7ac98322d1cc24a4cfda403da
Hendrik du Toit <hendrik@brightedge.co.za>
2025-01-22 22:22:55 +0200

    Fix 03-CI Workflow

--- tests/test_dockercomposeexample.py
+++ tests/test_dockercomposeexample.py
@@ -1,23 +1,18 @@
-import subprocess
-import time
-
-import pytest
 import requests
 
-
-@pytest.fixture(scope="module", autouse=True)
-def docker_compose_up_down():
-    """Setup and teardown for Docker Compose."""
-    # Start Docker Compose
-    subprocess.run(["docker-compose", "up", "-d"], check=True)
-
-    # Wait for the services to be ready
-    time.sleep(10)  # Adjust time as needed based on your environment
-
-    yield
-
-    # Tear down Docker Compose
-    subprocess.run(["docker-compose", "down"], check=True)
+# @pytest.fixture(scope="module", autouse=True)
+# def docker_compose_up_down():
+#     """Setup and teardown for Docker Compose."""
+#     # Start Docker Compose
+#     subprocess.run(["docker-compose", "up", "-d"], check=True)
+#
+#     # Wait for the services to be ready
+#     time.sleep(10)  # Adjust time as needed based on your environment
+#
+#     yield
+#
+#     # Tear down Docker Compose
+#     subprocess.run(["docker-compose", "down"], check=True)
 
 
 def test_app_response():
